cfaec3a086a6892ec97796a7e5d8f5e997f4da80
Piotr Mankowski <pmanko@uw.edu>
2022-04-12 22:52:25 -0700

    Updated testing for offline use

--- .postman/run-tests-offline.sh
+++ .postman/run-tests-offline.sh
@@ -1,8 +1,22 @@
 #!/bin/bash
+mllpPort=3001
 
+
+# Postman Collections
 for collection in .postman/collections/*
 do
   file=`basename $collection`
   echo "Running $file"
   docker run -v ~/code/hie-botswana/.postman:/etc/newman -t postman/newman run "collections/$file" --environment="postman_env.moh.json"
+done
+
+# MLLP Messages
+
+for hl7File in messages/*
+do
+  echo "Testing $hl7File"
+
+  echo $hostname
+
+  echo -ne $msg | socat - TCP:$hostname:$port
 done
