79d28561cc197d1d87d60a875ac83459a4af0b27
Piotr Mankowski <pmanko@uw.edu>
2023-11-05 12:56:39 +0200

    Updates

--- mk.sh
+++ mk.sh
@@ -2,18 +2,13 @@
 
 ./build-image.sh
 
-#./instant package remove -n shared-health-record --env-file .env
-#./instant package remove -n client-registry-opencr --env-file .env
-#./instant package remove -n openhim-mediator-fhir-converter --env-file .env
-#./instant package remove -n reverse-proxy-traefik --env-file .env
-./instant package remove -n interoperability-layer-openhim --env-file .env
+./instant package remove -n shared-health-record --env-file .env
+# ./instant package remove -n client-registry-opencr --env-file .env
+# ./instant package remove -n fhir-datastore-hapi-fhir --env-file .env
+# ./instant package remove -n message-bus-kafka --env-file .env
 
+
+# ./instant package init -n message-bus-kafka --env-file .env
+# ./instant package init -n fhir-datastore-hapi-fhir --env-file .env
 # ./instant package init -n client-registry-opencr --env-file .env
-#./instant package init -n openhim-mediator-fhir-converter --env-file .env
-#./instant package init -n reverse-proxy-traefik --env-file .env
-./instant package init -n interoperability-layer-openhim --env-file .env
-#./instant package init -n shared-health-record --env-file .env
-#./instant package init -n openhim-mediator-omang-service --env-file .env
-
-
-# docker stack deploy         -c ./interoperability-layer-openhim/docker-compose.yml                  --with-registry-auth         openhim
+./instant package init -n shared-health-record --env-file .env
